0b96b1714513b5362c7837bf773155f309342db7
edurc10 <urlmagu@gmail.com>
2023-02-28 10:39:39 +0900

    res

--- apt_pro_v1/views.py
+++ apt_pro_v1/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from models import Post
-from forms import PostForm
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
--- my_settings.py
+++ my_settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'apt_pro.urls'
+ROOT_URLCONF = 'apt_pro_django.urls'
 
 TEMPLATES = [
     {
